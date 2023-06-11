417858fe6c584c6769fc3e4a1dc38bd962986693
Roberta Quiles <roberta@MacBook-Pro-de-Roberta.local>
2023-06-11 20:10:52 -0300

    Config linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,19 @@
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    "react-app",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "prettier",
   ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh", "prettier"],
   rules: {
-    'react-refresh/only-export-components': 'warn',
+    "react-refresh/only-export-components": "warn",
+    "react/jsx-no-target-blank": "off",
+    "prettier/prettier": ["error"],
   },
-}
+};
